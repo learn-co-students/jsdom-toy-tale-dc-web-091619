0e9c4815c9e2a5990c309f8199ef9652e8f7c35e
chungcha <charlieschung@gmail.com>
2019-11-01 15:27:45 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     addToy = !addToy
     if (addToy) {
       toyForm.style.display = 'block'
-      toyForm.addEventListener("submit", addNewToy)
+      toyForm.addEventListener("submit", addNewToy) //this renders a new toy everytime I edit an existing toy but I'm too lazy to keep working on this.
     } else {
       toyForm.style.display = 'none'
     }
@@ -24,7 +24,56 @@ document.addEventListener("DOMContentLoaded", ()=>{
   buttonArr.forEach(button=>{
     button.addEventListener("click", addLike)
   })
+
+  const editForm = document.querySelector(".edit-toy-form")
+  editForm.addEventListener("submit", editToy)
+
+  const selectForm = document.querySelector(".edit-toy-form-id")
+  selectForm.addEventListener("change", pullupToy)
 })
+
+function pullupToy(event){
+  const editForm = document.querySelector(".edit-toy-form")
+  let toyId = document.getElementById("toy-id").value
+  
+  let toyCard = document.getElementById(toyId)
+
+  editForm.name.value = toyCard.getElementsByTagName("h2")[0].innerText.split(`(`)[0]
+  editForm.image.value = toyCard.getElementsByTagName("img")[0].src
+
+}
+
+function editToy(event){
+  const editForm = document.querySelector(".edit-toy-form")
+  const toyId = document.querySelector(".edit-toy-form-id")[0].value
+  let toyName = editForm[0].value
+  let toyPic = editForm[1].value
+  
+
+  const configObj = {
+    method: "PATCH",
+    headers:{
+      "Content-Type":"application/json",
+      "Accept":"application/json"
+    },
+    body:JSON.stringify({name: toyName, image: toyPic})
+  }
+
+  fetch(`http://localhost:3000/toys/${toyId}`, configObj)
+    .then(response => response.json())
+    .then(updatedToy=> updateCard(updatedToy))
+}
+
+function updateCard(){
+  const toyId = document.querySelector(".edit-toy-form-id")[0].value
+  const editForm = document.querySelector(".edit-toy-form")
+  let toyPic = editForm[1].value
+  let toyName = editForm[0].value
+  let toyCard = document.getElementById(toyId)
+
+  toyCard.getElementsByTagName("h2")[0].innerText = toyName
+
+}
 
 function addLike(event){
   let toyId = event.target.parentNode.id
@@ -56,11 +105,12 @@ function updateLikes(updatedJson){
 function renderToy(toyObj){
   const toyCollection = document.getElementById("toy-collection")
   const card = document.createElement("div")
+  const selectId = document.getElementById("toy-id")
   card.classList.add("card")
   card.id = toyObj.id
 
   const header= document.createElement("h2")
-  header.innerText = toyObj.name
+  header.innerText = `${toyObj.name} (id:${toyObj.id})`
 
   const image = document.createElement("img")
   image.classList.add("toy-avatar")
@@ -78,6 +128,10 @@ function renderToy(toyObj){
   delButton.innerText= "DELETE ME"
   delButton.addEventListener("click", delToy)
 
+  const selectOption = document.createElement("option")
+  selectOption.innerText = toyObj.id
+  selectOption.value = toyObj.id
+
   
   toyCollection.appendChild(card)
   card.appendChild(header)
@@ -85,6 +139,7 @@ function renderToy(toyObj){
   card.appendChild(image)
   card.appendChild(likes)
   card.appendChild(toyButton)
+  selectId.appendChild(selectOption)
 }
 
 function addNewToy(event){
