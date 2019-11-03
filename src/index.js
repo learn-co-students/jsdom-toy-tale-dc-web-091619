84553a90ee788bb5d34c0a06f7649a6cf11ad9d3
Graham Flaspoehler <grahamflas@gmail.com>
2019-11-03 08:25:59 -0500

    increaseLike() built

--- src/index.js
+++ src/index.js
@@ -13,4 +13,101 @@ document.addEventListener("DOMContentLoaded", ()=>{
     }
   })
 
+  fetchToys();
+  getForm().addEventListener('submit', submitHandler)
+
 })
+
+function fetchToys() {
+  fetch("http://localhost:3000/toys")
+    .then( response => response.json() )
+    .then( toyArray => {
+      toyArray.forEach( toy => renderToy(toy) )
+    })
+}
+
+function renderToy(toy) {
+  let toyCard = document.createElement('div')
+  toyCard.classList.add('card')
+
+  let toyName = document.createElement('h2')
+  toyName.innerText = toy.name
+  
+  let toyImg = document.createElement('img')
+  toyImg.classList.add("toy-avatar")
+  toyImg.src = toy.image
+
+  let toyLikes = document.createElement('p')
+  toyLikes.id = `toy-${toy.id}-likes`
+  toyLikes.innerText = toy.likes
+
+  let likeBtn = document.createElement('button')
+  likeBtn.id = `toy-${toy.id}-likebtn`
+  likeBtn.innerText = "♥"
+  likeBtn.addEventListener('click', increaseLike)
+  
+  toyCard.append(toyName, toyImg, toyLikes, likeBtn)
+  collectionContainer().append(toyCard)
+}
+
+function submitHandler(){
+  event.preventDefault()
+  let newName = event.currentTarget.name.value
+  let newImage = event.currentTarget.image.value
+  let likes = 0
+  let data = {
+    name: newName,
+    image: newImage,
+    likes: likes
+  }
+
+  fetch("http://localhost:3000/toys", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+    .then( response => response.json() )
+    .then( newToy => renderToy(newToy) )
+    .catch( () => alert("Whoops, something went wrong on our end") )
+
+  event.currentTarget.reset()
+}
+
+/* When a user clicks the like button, make a PATCH request to the server incrementing the current value of like
+Rerender the toy with the response.
+*/
+function increaseLike(){
+  let currentToyId = parseInt(event.currentTarget.id.split("-")[1]);
+  let currentLikes = parseInt( document.getElementById(`toy-${currentToyId}-likes`).innerText )
+  newLikes = currentLikes + 1
+  debugger
+  fetch(`http://localhost:3000/toys/${currentToyId}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({likes: newLikes})
+  })
+    .then( response => response.json() )
+    .then( data => console.log(data) )
+
+}
+
+//_______________DOM nodes_______________
+function collectionContainer(){
+  return document.getElementById('toy-collection')
+} 
+
+function getForm(){
+  return document.querySelector('.add-toy-form')
+}
+
+
+
+
+
+ 
